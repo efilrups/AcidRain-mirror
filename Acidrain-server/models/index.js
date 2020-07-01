ddba47320f8e3d24dcfcaf1d50e245bcbdc6dc13
oyeon-kwon <61301574+oyeon-kwon@users.noreply.github.com>
2020-07-01 14:52:51 +0900

    In progress server

---
+++ Acidrain-server/models/index.js
@@ -0,0 +1,47 @@
+const db = require("../db");
+
+module.exports = {
+    mypage: {
+        get: function (callback) {
+            
+        },
+        post: function (params, callback) {
+            
+        }
+    },
+    signup: {
+        get: function (callback){
+
+        }
+    },
+    selectstage: {
+        get: function (callback){
+
+        }
+    },
+    playstage: {
+        get: function (callback){
+
+        }
+    },
+    rank: {
+        get: function (callback){
+
+        }
+    },
+    login: {
+        post: function (params, callback){
+
+        }
+    },
+    guest: {
+        post: function (params, callback){
+
+        }
+    },
+    gameover: {
+        post: function (params, callback){
+
+        }
+    }
+}
