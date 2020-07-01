1912cd4ee06826ef1bcf83068fdf83f2792e8753
iamnayeon <nana.nayeonk@gmail.com>
2020-07-01 16:11:10 +0900

    Set react route(in progress)

---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as login } from './login'
+export { default as nav } from './nav'
+export { default as playStage } from './playStage'
+export { default as selectStage } from './selectStage'
