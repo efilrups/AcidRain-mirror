6ac88d1b42b9a5041649ef5a2ab5ba44a29bbd71
chanul94 <cksdnfwkd@naver.com>
2020-07-04 18:33:56 +0900

    Fix origin

--- server/basic-server.js
+++ server/basic-server.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-      origin: ['http://localhost:3001'],
+      origin: ['http://localhost:3000'],
       methods: ['GET', 'POST'],
       credentials: true
     })
