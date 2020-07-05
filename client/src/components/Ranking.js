713012395d9939952d8f010c912e508523ee1318
rup_efilcondrahkk <lgm0614@naver.com>
2020-07-06 00:19:22 +0900

    Fix class -> className in Ranking(title-bar-controls)

--- client/src/components/Ranking.js
+++ client/src/components/Ranking.js
@@ -29,14 +29,14 @@ class Ranking extends Component {
     }
 
     render() {
-        //react-bootstrap-table을 이용하여 table을 구현, data를 지정하고 키 값을 지정하면 저절로 표를 구현할 수 있음 
+        //react-bootstrap-table을 이용하여 table을 구현, data를 지정하고 키 값을 지정하면 저절로 표를 구현할 수 있음
         return (
 
             <div className="window Ranking-window">
 
                 <div className="window-body">
-          
-                    <div class="title-bar-controls">
+
+                    <div className="title-bar-controls">
                         <button className="closeButton" aria-label="Close" onClick={()=>{
                             this.props.history.goBack()
                         }}></button>
@@ -46,7 +46,7 @@ class Ranking extends Component {
 
                     <fieldset>
                     <div className="field-row" style={{ justifyContent: 'center' }}>
-                        <BootstrapTable data={this.state.rankingOfTop10} 
+                        <BootstrapTable data={this.state.rankingOfTop10}
                         bordered={false}
                         headerStyle={{'borderBottom':'black 0.05rem solid', 'marginBottom':'1rem', 'marginTop':'1rem', 'paddingBottom':'1rem'}}
                         containerStyle={{ 'borderBottom': 'black solid 0.05rem', 'paddingBottom':'1rem'}}
@@ -75,4 +75,3 @@ class Ranking extends Component {
 }
 
 export default withRouter(Ranking)
-
